3ea194a782ce32a6a0c6aaeff1606c9eb7e7f496
Zain Jaffri <zainjaffri2@gmail.com>
2023-11-14 17:26:06 -0400

    feat: Fixed jest errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+// import * as axios from "axios";
 import App from "./App";
+
+// Mock out all top level functions, such as get, put, delete and post:
+jest.mock("axios", () => ({
+	get: async () => new Promise(() => { }),
+	post: async () => new Promise(() => { }),
+}));
 
 test("renders learn react link", () => {
 	render(
--- src/IzServicesComponents/EditInvoice.tsx
+++ src/IzServicesComponents/EditInvoice.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { MyContext } from "../MyContext";
-// import Search from "./Search";
 
 const services = [
 	{ name: "Address Change", price: 10.0 },
