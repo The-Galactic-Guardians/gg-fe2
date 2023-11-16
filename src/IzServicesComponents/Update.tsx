e6acb46f029d8840b53767a3af8d3bdcb64aed6c
Zain Jaffri <zainjaffri2@gmail.com>
2023-11-16 09:57:46 -0400

    feat: Update Search Component

--- src/IzServicesComponents/Update.tsx
+++ src/IzServicesComponents/Update.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
 // import axios from "axios";
 // import { MyContext } from "../MyContext";
 
 function Update() {
-	// const navigate = useNavigate();
-
 	return (
 		<div className="flex flex-col items-center justify-start min-h-screen">
 			<h2 className="mt-10 text-4xl">
@@ -15,12 +12,12 @@ function Update() {
 			<div className="mt-10">
 				<input
 					type="text"
-					placeholder="Search by Invoice Number"
+					placeholder="Update by Invoice Number"
 					className="w-96 h-10 p-2 text-base border-2 border-gray-300 rounded mr-4"
 				/>
 				<button
 					type="button"
-					className="bg-green-500 text-white font-bold py-2 px-8 rounded shadow border-2 border-green-500 hover: bg-transparent hover:text-green-500 transition-all duration-300"
+					className="bg-green-500 text-white font-bold py-1 px-4 rounded shadow border-2 border-green-500 hover: bg-transparent hover:text-green-500 transition-all duration-300"
 				>
 					Update
 				</button>
